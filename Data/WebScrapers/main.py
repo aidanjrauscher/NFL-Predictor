40f094e98eedad497fa97853a7c2086a077fdac0
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-01-12 20:05:26 -0500

    scraper data

--- Data/WebScrapers/main.py
+++ Data/WebScrapers/main.py
@@ -4,55 +4,43 @@ import pandas as pd
 DRIVER_PATH = 'C:/Projects/Selenium/chromedriver/chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-driver.get('https://www.pro-football-reference.com/years/2020/games.htm')
+driver.get('https://www.pro-football-reference.com/years/2003/index.htm')
 
-weekNum = driver.find_elements_by_xpath("//th[@class='right '][@data-stat='week_num']")
-winnerName = driver.find_elements_by_xpath('//td[@data-stat="winner"]')
-loserName = driver.find_elements_by_xpath('//td[@data-stat="loser"]')
-ptsWinner = driver.find_elements_by_xpath('//td[@data-stat="pts_win"]')
-ptsLoser = driver.find_elements_by_xpath('//td[@data-stat="pts_lose"]')
-location = driver.find_elements_by_xpath('//td[@data-stat="game_location"]')
-dayOfWeek = driver.find_elements_by_xpath('//td[@data-stat="game_day_of_week"]')
-timeOfDay = driver.find_elements_by_xpath('//td[@data-stat="gametime"]')
+name = driver.find_elements_by_xpath('//td[@data-stat="team"]')
+gamesPlayed = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div/table/tbody/tr/td[@data-stat='g']")
+pts = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div/table/tbody/tr/td[@data-stat='points']")
 
-weeks = []
-winners = []
-winnerPts = []
-loserPts = []
-losers = []
-locations = []
-days = []
-times = []
+yrds = driver.find_elements_by_xpath("//*[@id='team_stats']/tbody/tr/td[@data-stat='total_yards']")
+npya = driver.find_elements_by_xpath("//*[@id='team_stats']/tbody/tr/td[@data-stat ='pass_net_yds_per_att']")
+nrya = driver.find_elements_by_xpath("//*[@id='team_stats']/tbody/tr/td[@data-stat='rush_yds_per_att']")
 
-for i in range(len(weekNum)):
-    week = weekNum[i].text
-    try:
-        isNum = int(week)
-        weeks.append(week)
-        winners.append(winnerName[i].text)
-        losers.append(loserName[i].text)
-        winnerPts.append(ptsWinner[i].text)
-        loserPts.append(ptsLoser[i].text)
-        locations.append(location[i].text)
-        days.append(dayOfWeek[i].text)
-        times.append(timeOfDay[i].text)
+names = []
+avgPts = []
+avgYrds = []
+NPYperA = []
+NRYperA = []
 
-    except:
-        pass
+
+
+for i in range(len(pts)):
+    games = int(gamesPlayed[i].text)
+    names.append(name[i].text)
+    avgPts.append(int(pts[i].text)/games)
+    avgYrds.append(int(yrds[i].text)/games)
+    NPYperA.append(float(npya[i].text))
+    NRYperA.append(float(nrya[i].text))
+
 driver.close()
 
-DF = pd.DataFrame ({
-    'Week': weeks,
-    'Winner': winners,
-    'Loser' : losers,
-    'PtsW': winnerPts,
-    'PtsL': loserPts,
-    'Location' : locations,
-    'Day': days,
-    'Time': times,
+StatDataFrame1995 = pd.DataFrame ({
+    'Name': names,
+    'Avg Points': avgPts,
+    'Avg Yards': avgYrds,
+    'NPY/A': NPYperA,
+    'NRY/A': NRYperA,
 })
 
-path = r'C:\Projects\NFLPredictor\Data\Test-Data'
-name = "\Games2020.csv"
+path = r'C:\Projects\NFLPredictor\Data\Train-Data\OffensiveStats'
+name = "\OffensiveStats2003.csv"
 location = path + name
-DF.to_csv(location, index=False)
+StatDataFrame1995.to_csv(location, index=False)
