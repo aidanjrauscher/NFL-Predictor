8431cfa20e1c4108548969c53c4e0877f58f9383
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-01-13 00:50:48 -0500

    combined csv

--- Data/WebScrapers/main.py
+++ Data/WebScrapers/main.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 import pandas as pd
 
-df1 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\OffensiveStats\OffensiveStats2020.csv')
+df1 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\OffensiveStats\OffensiveStats1994.csv')
 dfa = df1.sort_values(by='Name')
 #dfa.set_index('Name', drop=True)
-df2 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\OffConvStats\OffConv2020.csv')
+df2 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\OffConvStats\OffConv1994.csv')
 dfb = df2.sort_values(by=['Name'])
 #dfb.set_index('Name', drop=True)
-df3 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\DefensiveStats\DefensiveStats2020.csv')
+df3 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\DefensiveStats\DefensiveStats1994.csv')
 dfc = df3.sort_values(by=['Name'])
 #dfc.set_index('Name', drop=True)
-df4 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\DefConvStats\DefConv2020.csv')
+df4 = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\DefConvStats\DefConv1994.csv')
 dfd = df4.sort_values(by=['Name'])
 #dfd.set_index('Name', drop=True)
 
@@ -22,6 +22,6 @@ allStats = pd.merge(allStats.set_index('Name', drop=True),df4.set_index('Name', 
 
 
 location = r'C:\Projects\NFLPredictor\Data\Test-Data\CombinedStats'
-name = '\CombinedStats2020'
+name = '\CombinedStats1994.csv'
 path = location + name
 allStats.to_csv(path, index=False)
