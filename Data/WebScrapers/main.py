e038ed4698971badd5c113177342e087b7be758f
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-01-12 21:01:53 -0500

    scraper data

--- Data/WebScrapers/main.py
+++ Data/WebScrapers/main.py
@@ -4,14 +4,14 @@ import pandas as pd
 DRIVER_PATH = 'C:/Projects/Selenium/chromedriver/chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-driver.get('https://www.pro-football-reference.com/years/2013/index.htm')
+driver.get('https://www.pro-football-reference.com/years/2020/index.htm')
 
-name = driver.find_elements_by_xpath('/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat="team"]/a')
-gamesPlayed = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat='g']")
-pts = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat='points']")
-yrds = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat='total_yards']")
-npya = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat ='pass_net_yds_per_att']")
-nrya = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[9]/div[3]/div[2]/table/tbody/tr/td[@data-stat='rush_yds_per_att']")
+name = driver.find_elements_by_xpath('/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat="team"]/a')
+gamesPlayed = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat='g']")
+pts = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat='points']")
+yrds = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat='total_yards']")
+npya = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat ='pass_net_yds_per_att']")
+nrya = driver.find_elements_by_xpath("/html/body/div[2]/div[5]/div[8]/div[3]/div[2]/table/tbody/tr/td[@data-stat='rush_yds_per_att']")
 
 
 names = []
@@ -40,7 +40,7 @@ StatDataFrame1995 = pd.DataFrame ({
     'NRY/A': NRYperA,
 })
 
-path = r'C:\Projects\NFLPredictor\Data\Train-Data\OffensiveStats'
-name = "\OffensiveStats2013.csv"
+path = r'C:\Projects\NFLPredictor\Data\Test-Data\OffensiveStats'
+name = "\OffensiveStats2020.csv"
 location = path + name
 StatDataFrame1995.to_csv(location, index=False)
