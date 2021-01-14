046e24cb9f5d6458c9d1bac633471e9e67502460
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-01-14 00:12:54 -0500

    data fix

--- Data/WebScrapers/merge23years.py
+++ Data/WebScrapers/merge23years.py
@@ -6,8 +6,10 @@ df3 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Train-Data/Matchups/MatchUp199
 df4 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Train-Data/Matchups/MatchUp1999.csv')
 df5 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Train-Data/Matchups/MatchUp2000.csv')
 df6 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Train-Data/Matchups/MatchUp2001.csv')
-df7 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/WebScrapers/testNOPERCENTAGES.csv')
+df7 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Train-Data/Compiled/test.csv')
+df8 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Test-Data/Matchups/MatchUp2019.csv')
+df9 = pd.read_csv(r'C:/Projects/NFLPredictor/Data/Test-Data/Matchups/MatchUp2020.csv')
 
-tet = pd.concat([df1, df2, df3, df4, df5, df6, df7],axis=0)
+tet = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9],axis=0)
 
-tet.to_csv('test.csv', index=False)
+tet.to_csv(r'C:/Projects/NFLPredictor/Model/AidansModels/playofftest.csv', index=False)
