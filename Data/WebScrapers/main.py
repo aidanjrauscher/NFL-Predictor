2fa6bd93e73f81e4bcf4282bda916976ac8f1443
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-01-15 22:34:11 -0500

    playoff prediction

--- Data/WebScrapers/main.py
+++ Data/WebScrapers/main.py
@@ -1,161 +1,26 @@
 import pandas as pd
 import random
 
-#read csv files:
 
-statsDF = pd.read_csv(r'C:\Projects\NFLPredictor\Data\Test-Data\CombinedStats\CombinedStats2020.csv')
-
-
-
-team1 = []
-team1outcome = []
-team1score = []
-team1location = []
-t1AvgPts = []
-t1AvgYads = []
-t1NPYA = []
-t1NRYA = []
-t13rd = []
-t14th = []
-
-t1AvgPtsDef = []
-t1AvgYadsDef = []
-t1NPYADef = []
-t1NRYADef = []
-t13rdDef = []
-t14thDef = []
-
-team2 = []
-team2outcome = []
-team2score = []
-team2location = []
-t2AvgPts = []
-t2AvgYads = []
-t2NPYA = []
-t2NRYA = []
-t23rd = []
-t24th = []
-
-t2AvgPtsDef = []
-t2AvgYadsDef = []
-t2NPYADef = []
-t2NRYADef = []
-t23rdDef = []
-t24thDef = []
+modelNum = 2019
+year = 2019
 
 
+path1 = r'C:\Projects\NFLPredictor\Model\AidansModels\RandomForest\test2019d.csv'
+modelDF = pd.read_csv(path1)
 
-games = [['Buffalo Bills', 'Indianapolis Colts'],['Los Angeles Rams', 'Seattle Seahawks'],
-         ['Tampa Bay Buccaneers', 'Washington Football Team'],['Baltimore Ravens' ,'Tennessee Titans'],
-         ['New Orleans Saints','Chicago Bears'],['Cleveland Browns','Pittsburgh Steelers']]
-def dePercentify(per):
-    num = per.replace('%', '')
-    return float(num)/100
+path2 = r'C:\Projects\NFLPredictor\Data\Test-Data\Matchups\MatchUp2019.csv'
+gameDF = pd.read_csv(path2)
 
-for i in range(len(games)):
-    teamInfo = []
-    #randomly assigns team 1 and team 2
-    first = random.randrange(0, 2)
-    a = games[i][0]
-    b = games[i][1]
-    location = ['','@','@','@','','@']
-    teamInfo.append([a, statsDF.loc[statsDF['Name']==a], 1, 0])
-    teamInfo.append([b, statsDF.loc[statsDF['Name']==b], 0, 0])
-
-    if location[i] == '@':
-        teamInfo[0].append('Away')
-        teamInfo[1].append('Home')
+right = 0
+wrong = 0
+for i in range(len(modelDF["T1"])):
+    if gameDF["T1 Outcome"][i] == modelDF['T1 Outcome'][i]:
+        right+=1
     else:
-        teamInfo[1].append('Away')
-        teamInfo[0].append('Home')
+        wrong+=1
 
-    #collect all team 1 and team 2 information/stats for each game
-    teamOne = teamInfo[first][0]
-    teamOneStats = teamInfo[first][1]
-    teamTwo = teamInfo[1-first][0]
-    teamTwoStats = teamInfo[1-first][1]
+print(str(round(((right/(right+wrong))*100),3))+"% Accurate")
 
-    team1.append(teamOne)
-    team1outcome.append(teamInfo[first][2])
-    team1score.append(teamInfo[first][3])
-    team1location.append(teamInfo[first][4])
-
-    t1AvgPts.append(teamOneStats['Avg Points'].item())
-    t1AvgYads.append(teamOneStats['Avg Yards'].item())
-    t1NPYA.append(teamOneStats['NPY/A'].item())
-    t1NRYA.append(teamOneStats['NRY/A'].item())
-    t13rd.append(dePercentify(teamOneStats['3rd%_x'].item()))
-    t14th.append(dePercentify(teamOneStats['4th%_x'].item()))
-
-    t1AvgPtsDef.append(teamOneStats['Avg Points Let'].item())
-    t1AvgYadsDef.append(teamOneStats['Avg Yards Let'].item())
-    t1NPYADef.append(teamOneStats['NPY/A Let'].item())
-    t1NRYADef.append(teamOneStats['NRY/A Let'].item())
-    t13rdDef.append(dePercentify(teamOneStats['3rd%_y'].item()))
-    t14thDef.append(dePercentify(teamOneStats['4th%_y'].item()))
-
-    team2.append(teamTwo)
-    team2outcome.append(teamInfo[1-first][2])
-    team2score.append(teamInfo[1-first][3])
-    team2location.append(teamInfo[1 - first][4])
-
-    t2AvgPts.append(teamTwoStats['Avg Points'].item())
-    t2AvgYads.append(teamTwoStats['Avg Yards'].item())
-    t2NPYA.append(teamTwoStats['NPY/A'].item())
-    t2NRYA.append(teamTwoStats['NRY/A'].item())
-    t23rd.append(dePercentify(teamTwoStats['3rd%_x'].item()))
-    t24th.append(dePercentify(teamTwoStats['4th%_x'].item()))
-
-    t2AvgPtsDef.append(teamTwoStats['Avg Points Let'].item())
-    t2AvgYadsDef.append(teamTwoStats['Avg Yards Let'].item())
-    t2NPYADef.append(teamTwoStats['NPY/A Let'].item())
-    t2NRYADef.append(teamTwoStats['NRY/A Let'].item())
-    t23rdDef.append(dePercentify(teamTwoStats['3rd%_y'].item()))
-    t24thDef.append(dePercentify(teamTwoStats['4th%_y'].item()))
-
-
-
-everything = pd.DataFrame ({
-    'T1': team1,
-    'T1 Outcome': team1outcome,
-    'T1 Score': team1score,
-    'T1 Location': team1location,
-    'T1 Avg Points': t1AvgPts,
-    'T1 Avg Yards': t1AvgYads,
-    'T1 NPY/A': t1NPYA,
-    'T1 NRY/A':t1NRYA,
-    'T1 3rd': t13rd,
-    'T1 4th': t14th,
-    'T1 Avg Points Def' : t1AvgPtsDef,
-    'T1 Avg Yards Def': t1AvgYadsDef,
-    'T1 NPY/A Def': t1NPYADef,
-    'T1 NRY/A Def': t1NRYADef,
-    'T1 3rd Def': t13rdDef,
-    'T1 4th Def': t14thDef,
-
-    'T2': team2,
-    'T2 Outcome': team2outcome,
-    'T2 Score': team2score,
-    'T2 Location':  team2location,
-    'T2 Avg Points': t2AvgPts,
-    'T2 Avg Yards': t2AvgYads,
-    'T2 NPY/A': t2NPYA,
-    'T2 NRY/A':t2NRYA,
-    'T2 3rd': t23rd,
-    'T2 4th': t24th,
-    'T2 Avg Points Def' : t2AvgPtsDef,
-    'T2 Avg Yards Def': t2AvgYadsDef,
-    'T2 NPY/A Def': t2NPYADef,
-    'T2 NRY/A Def': t2NRYADef,
-    'T2 3rd Def': t23rdDef,
-    'T2 4th Def': t24thDef,
-})
-
-
-
-location = r'C:\Projects\NFLPredictor\Model\AidansModels\RandomForest'
-name = '\MatchUp2020WildCard.csv'
-path = location + name
-everything.to_csv(path, index=False)
 
 
